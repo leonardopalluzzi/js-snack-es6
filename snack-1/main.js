01b6263f55b6309a42fe06767cfad28804ff0378
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-21 15:23:29 +0100

    add card structure and html element recover

---
+++ snack-1/main.js
@@ -0,0 +1,23 @@
+//Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore
+
+//recover HTML assets
+const BikeNameEl = document.querySelector('.card-title');
+const BikeWeighEl = document.querySelector('.bike_w');
+const bikes = [
+    {
+        bikeName: 'super mountain',
+        bikeWeight: 80,
+    }, {
+        bikeName: 'ulta light street',
+        bikeWeight: 20,
+    }, {
+        bikeName: 'all round',
+        bikeWeight: 35,
+    }
+];
+
+
+function getLighterBike(arr) {
+
+}
